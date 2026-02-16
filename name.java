43992c54b2923eb409668fbc4d8e55bf51d43fea
niraj-upadhyay <nirajupadhyay2024@gmail.com>
2026-02-16 14:51:42 +0530

    First Java practice

---
+++ name.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class name {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("WELCOME " + name + " to KG Coding");
+
+    }
+
+}
